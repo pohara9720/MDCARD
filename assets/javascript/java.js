2bd79fe455e21f30efcdc3495ea2ce3a0435ebd0
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-05-14 17:36:34 -0700

    asdasd

---
+++ assets/javascript/java.js
@@ -0,0 +1,7 @@
+
+
+    $(".btn").on("click",function(){
+      $(".image").attr("src","assets/images/mdme.jpg");
+      $("#text").html("Happy Mothers Day! You're the strongest woman I know, and I respect who you are and the person you've become. I know its not always easy but you kids are always here for you and we will always love you :) ! You have helped me through so much and I hope to one day repay you. I love you! ");
+
+    });
